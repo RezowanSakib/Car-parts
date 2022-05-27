78d714a75c255a49bc5d9f0db3cd0fa88b4fc47f
Sakib Md Rezowan <sakibmdrezowan@gmail.com>
2022-05-27 20:05:09 +0600

    order can be placed

--- src/Components/Purchase/Purchase.js
+++ src/Components/Purchase/Purchase.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
-
+import { useForm } from "react-hook-form";
 const Purchase = () => {
   const { partID } = useParams();
   const [product, setProduct] = useState({});
-  const {picture,balance}=product
+  const { picture, balance } = product;
   const [user, loading, error] = useAuthState(auth);
   const handlePurcing = (event) => {
     event.preventDefault();
@@ -18,93 +18,122 @@ const Purchase = () => {
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, []);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) =>{
+    const url = `http://localhost:5000/order`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-              <div className="form-control">
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <label className="label">
                   <span className="label-text">Customer Name</span>
                 </label>
                 <input
-                  type="text"
-                  name="name"
-                  disabled
                   value={user?.displayName || ""}
                   className="input input-bordered w-full max-w-xs"
+                  {...register("name", { required: true, maxLength: 20 })}
                 />
-              </div>
-              <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
-                  name="email"
-                  disabled
+                  placeholder="Your Email"
                   value={user?.email || ""}
                   className="input input-bordered w-full max-w-xs"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is Required",
+                    },
+                    pattern: {
+                      value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                      message: "Provide a valid Email",
+                    },
+                  })}
                 />
-              </div>
-              <div className="form-control">
                 <label className="label">
                   <span className="label-text">Phone</span>
                 </label>
                 <input
-          type="text"
-          name="phone"
-          placeholder="Phone Number"
-          className="input input-bordered w-full max-w-xs"
-        />
-              
-              </div>
-              <div className="form-control">
+                  type="text"
+                  placeholder="Phone"
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("phone", {
+                    required: {
+                      value: true,
+                      message: "Phone number is Required",
+                    },
+                    minLength: {
+                      value: 11,
+                      message: "enter a valid phone number",
+                    },
+                  })}
+                />{" "}
                 <label className="label">
                   <span className="label-text">Quantity</span>
                 </label>
                 <input
-          type="text"
-          name="quantity"
-          placeholder="Quantity"
-          value={product.minimum}
-          className="input input-bordered w-full max-w-xs"
-        />
-              
-              </div>
-              <div className="form-control">
+                  type="text"
+                  placeholder="Quantity"
+                  value={product.minimum}
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("quantity", { min: 18, max: 99 })}
+                  
+                />
                 <label className="label">
                   <span className="label-text">Address</span>
                 </label>
                 <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          className="input input-bordered w-full max-w-xs"
-        />
-              
-              </div>
-              <input
-          type="submit"
-          value="Submit"
-          className="btn btn-secondary w-full max-w-xs"
-        />
+                  type="text"
+                  placeholder="Address"
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("address", { pattern: /^[A-Za-z]+$/i })}
+                />
+                <input
+                  className="btn btn-secondary w-full max-w-xs"
+                  type="submit"
+                />
+              </form>
             </div>
           </div>
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Purchase now!</h1>
-           
             <figure className="px-10 pt-10">
-        <img src="https://api.lorem.space/image/movie?w=260&h=400" alt="Shoes" className="rounded-xl" />
-      </figure> <h1 className="text-3xl font-bold">{product.Name}</h1>
+              <img
+                src="https://api.lorem.space/image/movie?w=260&h=400"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <h1 className="text-3xl font-bold">{product.Name}</h1>
             <p className="py-6">{product.about}</p>
             <h3>Available :{product.available}</h3>
             <p className="py-6">Price :{balance}</p>
           </div>
         </div>
       </div>
-     
     </div>
   );
 };
